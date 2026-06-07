a890fab0cf5189403d32287fc9d0b3b5c4e4c791
David <davidsetty1+github@gmail.com>
2026-06-07 20:46:49 +0100

    Comitting code

---
+++ SpecCalc/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SpecCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number: ");
+            var inputValue = Console.ReadLine();
+
+            if (int.TryParse(inputValue, out var inputAsInt)) {
+                OddOrEven(inputAsInt);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void OddOrEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is prime.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is odd.");
+            }
+        }
+    }
+}
